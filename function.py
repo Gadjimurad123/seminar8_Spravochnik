4f3c48a034a6861a4c3cdae9a814f5d4b7f1b4b5
Gadjimurad Aliev <BeanOfficial@list.ru>
2023-07-22 17:47:43 +0300

    Создал файл function

--- function.py
+++ function.py
@@ -1,12 +1,3 @@
-separator = ['.', ';', ',', '-']
-
-
-def data_remake(data):
-    global separator
-    index_separator = data.split(separator[[i in data for i in separator].index(True)])
-    return index_separator
-
-
 def delete():
     printdata()
     answer = int(input('___________________________\n'
@@ -19,7 +10,7 @@ def delete():
 
     with open(f'db/data{answer}.txt', 'r', encoding='utf-8') as file:
         data = file.readlines()
-        number = int(data_remake(data[-1])[0])
+        number = int(data[-1].split(';')[0])
 
     number_row = int(input("___________________________\n"
                            f"Отлично! Будем удалять данные из {answer}-файла.\n"
@@ -33,7 +24,7 @@ def delete():
     count = 1
     result = list()
     for i in range(number - 1):
-        row = f'{count};' + data[i][data[i].index(separator[[i in data for i in separator].index(True)]) + 1:]
+        row = f'{count};' + data[i][data[i].index(';') + 1:]
         count += 1
         result.append(row)
     with open(f'db/data{answer}.txt', 'w', encoding='utf-8') as file:
@@ -43,11 +34,7 @@ def delete():
           'Удаление успешно завершено!')
 
 
-razdelitel = ''
-
-
 def add():
-    global razdelitel
     printdata()
     answer = int(input('___________________________\n'
                        'Выберите файла, в который Вы хотите добавить строку: '))
@@ -61,21 +48,29 @@ def add():
           '|')
     name = input("|\n| Введите имя: ")
     surname = input("|\n| Введите фамилию: ")
-    phone = input("|\n| Введите номер телефона: ")  # При необходимости можно добавить проверку на телефон с помощью регулярных выражений
+    phone = input("|\n| Введите номер телефона: ")  # При необходимости можно добавить проверку на телефон
     city = input("|\n| Введите город: ")
-    razdelitel = input("|\n| Введите разделитель(  - . ; ,  ): ")
-    with open(f'db/data{answer}.txt', 'r', encoding='utf-8') as file:
-        data = file.readlines()
-        if data:
-            number = int(data_remake(data[-1])[0])
-        else:
-            number = 0
-    with open(f'db/data{answer}.txt', 'w', encoding='utf-8') as file:
-        file.writelines(data + [f'{number + 1}{razdelitel}{name}{razdelitel}{surname}{razdelitel}{phone}{razdelitel}{city}\n'])
-
-    print('___________________________\n'
-          'Данные успешно записаны!')
-
+    var = int(input(f"В каком формате Вы хотите записать данные?\n\n"
+                    f"1 Вариант:\n"
+                    f"{surname}-{name}-{phone}-{city}\n\n"
+                    f"2 Вариант:\n"
+                    f"{surname}:{name}:{phone}:{city}\n\n"
+                    f"3 вариант:\n"
+                    f"{surname}/{name}/{phone}/{city}\n\n"
+                    f"Выберите номер формата: "))
+    
+    while var != 1 and var != 2 and var !=3:
+        print('Неправильно выбран номер формата')
+        var = int(input("Введите номер формата: "))
+    if var == 1:
+        with open('db/data1.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{name}-{surname}-{phone}-{city}\n\n')
+    if var == 2:
+        with open('db/data2.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{name}:{surname}:{phone}:{city}\n\n')
+    else:
+        with open('db/data3.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{name}/{surname}/{phone}/{city}\n\n')
 
 def change():
     printdata()
@@ -88,7 +83,7 @@ def change():
         loading()
     with open(f'db/data{answer}.txt', 'r', encoding='utf-8') as file:
         data = file.readlines()
-        number = int(data_remake(data[-1])[0])
+        number = int(data[-1].split(';')[0])
 
     number_row = int(input("___________________________\n"
                            f"Отлично! Будем изменять данные из {answer}-файла.\n"
@@ -137,17 +132,16 @@ def change():
         data = database[number_row - 1]
     print(data)
     if name is None:
-        name = data.split(separator[[i in data for i in separator].index(True)])[1]
+        name = data.split(';')[1]
     if surname is None:
-        surname = data.split(separator[[i in data for i in separator].index(True)])[2]
+        surname = data.split(';')[2]
     if phone is None:
-        phone = data.split(separator[[i in data for i in separator].index(True)])[3]
+        phone = data.split(';')[3]
     if city is None:
-        city = data.split(separator[[i in data for i in separator].index(True)])[4]
+        city = data.split(';')[4]
 
     with open(f'db/data{answer}.txt', 'w', encoding='utf-8') as file:
-        file.writelines(database[:number_row - 1] +
-                        [f"{data.split(separator[[i in data for i in separator].index(True)])[0]}{razdelitel}{name}{razdelitel}{surname}{razdelitel}{phone}{razdelitel}{city}"] +
+        file.writelines(database[:number_row] + [f"{data.split(';')[0]};{name};{surname};{phone};{city}"] +
                         database[number_row + 1:])
 
     print('___________________________\n'
@@ -199,7 +193,7 @@ def printdata():
         with open(f'db/data{i}.txt', 'r', encoding='utf-8') as file:
             print("___________________________\n"
                   f"Вывожу данные из {i}-го файла:")
-            data = [[j if '\n' not in j else j.split('\n')[0] for j in i.split(separator[[k in i for k in separator].index(True)])] for i in file.readlines()]
+            data = [[j if '\n' not in j else j.split('\n')[0] for j in i.split(';')] for i in file.readlines()]
             if len(data) == 0:
                 print("Файл пустой!")
             else:
@@ -224,7 +218,7 @@ def printdata():
 def check_numbers(answer):
     while answer < 1 or answer > 6:
         print("ERROR! Ошибка, скорее всего, Вы указали неправильное число.\n"
-              "Введите значение от 1 до 6.\n"
+              "Введите значение от 1 до 7.\n"
               "Выберите действие:\n"
               "___________________________\n"
               "1. Удалить запись.\n"
